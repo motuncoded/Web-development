873198f9e3001cea82b9c848f4d755f26d8c60f9
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-09-15 21:50:49 +0100

    Core modules-Day2

---
+++ 30daysoflearningnodejs/Day2/fileManager.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+//for reading file asnychrously
+
+fs.readFile('example.txt', 'utf8', (err, data)=>{
+    if (err) {
+        console.error(err)
+        return;
+    }
+    console.log('File content:', data)
+})
+
+//for writing file
+
+fs.writeFile('newExample.txt', 'This is a ne file', (err)=>{
+
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log('File is written')
+})
+
+// to append files
+
+fs.appendFile('nowExample.txt', '\nThis  is another file', (err) => {
+     if (err) {
+       console.error(err);
+       return;
+     }
+     console.log("Data appended successfully!");
+})
+//delete  files
+
+fs.unlink('delete.txt', (err) => {
+     if (err) {
+       console.error(err);
+       return;
+     }
+     console.log("File deleted successfully!");
+})
+
